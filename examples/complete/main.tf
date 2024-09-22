8b434b79006abfa32a13d7e6723e8de39455ce21
Thomas ILLIET <contact@thomas-illiet.fr>
2024-09-23 01:00:15 +0200

    Add example files

---
+++ examples/complete/main.tf
@@ -0,0 +1,27 @@
+module "kubernetes_ingress" {
+  source = "thomas-illiet/ingress/kubernetes"
+
+  ingress_name       = "my-ingress"
+  service_namespace  = "my-namespace"
+  domain_name        = "example.com"
+  ingress_class_name = "nginx"
+  cluster_issuer     = "letsencrypt-prod"
+
+  paths = [
+    {
+      path          = "/app"
+      service_name  = "app-service"
+      external_port = 80
+    },
+    {
+      path          = "/api"
+      service_name  = "api-service"
+      external_port = 8080
+    }
+  ]
+
+  tls = "my-custom-tls-secret"
+  annotations = {
+    "custom-annotation" = "value"
+  }
+}
---
+++ examples/minimal/main.tf
@@ -0,0 +1,13 @@
+module "kubernetes_ingress" {
+  source = "thomas-illiet/ingress/kubernetes"
+
+  ingress_name      = "test"
+  domain_name       = "example.com"
+  service_namespace = "demo"
+  paths = [
+    {
+      service_name  = "nginx"
+      external_port = "8080"
+    }
+  ]
+}
